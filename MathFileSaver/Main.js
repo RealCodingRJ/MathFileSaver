4f5364c67b7942117cf69c361912fd0ba1daac59
Ryan James <realcoder2023@gmail.com>
2025-04-02 20:41:59 -0400

    added checkmark to file as completed

--- MathFileSaver/Main.js
+++ MathFileSaver/Main.js
@@ -2,11 +2,11 @@ import fs from "fs";
 
 let randomNum1 = Math.floor(Math.random() * 10 + 1);
 let randomNum2 = Math.floor(Math.random() * 5 + 1);
-const text2 = `What is ${randomNum1} + ${randomNum2} = ${
-  randomNum1 + randomNum2 + "\n"
-}`;
+const text2 =
+  `${randomNum1} + ${randomNum2} = ${randomNum1 + randomNum2} ` + "\n" + "\n";
+const checkMark = "\u2713 ";
 
-fs.appendFileSync("./Main.txt", text2, (err) => {
+fs.appendFile("./Main.txt", checkMark + " " + text2, (err) => {
   if (err) {
     console.log(err);
   } else {
