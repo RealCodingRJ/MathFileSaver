eb0e8b4912531a9e8a661d5d8190ac8d1ddaa8c3
Ryan James <realcoder2023@gmail.com>
2025-04-02 21:22:59 -0400

    added check if append checkmark or not the dont add it to file

--- MathFileSaver/Main.js
+++ MathFileSaver/Main.js
@@ -1,23 +1,34 @@
-"use strict";
-
 import fs from "fs";
+import readline from "readline-sync";
 
 let randomNum1 = Math.floor(Math.random() * 10 + 1);
 let randomNum2 = Math.floor(Math.random() * 5 + 1);
 const text2 =
   `${randomNum1} + ${randomNum2} = ${randomNum1 + randomNum2} ` + "\n" + "\n";
-const checkMark = "\u2713 ";
 
 const data = fs.readFileSync("Data.json");
 
 const converted = JSON.parse(data);
 
-const stringStr = `${checkMark} Checked: ${text2}`;
+const question = readline.question("Do You Want to Add a Check  Y | N: ");
 
-fs.appendFile("./Main.txt", stringStr, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("written to file");
-  }
-});
+if (question.startsWith("y")) {
+  const checkMark = "\u2713 ";
+  const stringStr = `${checkMark} Checked: ${text2}`;
+
+  fs.appendFile("./Main.txt", stringStr, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("written to file");
+    }
+  });
+} else {
+  fs.appendFileSync("./Main.txt", text2, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("written to file");
+    }
+  });
+}
